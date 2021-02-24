7d31e067269b21fbede0f000f46229c54c63bf1d
oscarpm5 <47557468+oscarpm5@users.noreply.github.com>
2021-02-24 10:11:12 +0100

    Added Wwise AK folder

---
+++ Diamond Engine/Source Code/External Libraries/Wwise/AK/Plugin/ResonanceAudioFXFactory.h
@@ -0,0 +1,42 @@
+/*
+Copyright 2018 Google Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS-IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Copyright 2017 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+namespace vraudio {
+namespace wwise {
+
+// Static links for plug-in registrations.
+AK_STATIC_LINK_PLUGIN(ResonanceAudioRendererFX)
+AK_STATIC_LINK_PLUGIN(ResonanceAudioRoomEffectsFX)
+AK_STATIC_LINK_PLUGIN(ResonanceAudioRoomEffectsFXAttachment)
+
+}  // namespace wwise
+}  // namespace vraudio
