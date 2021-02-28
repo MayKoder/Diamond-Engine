45c44d2c20e6312a94e39ee597b686deda0cc85d
Bernat Casanas <47743689+BernatCasanas@users.noreply.github.com>
2021-02-28 16:18:45 +0100

    camera bug solved. camera done :+1:

--- Diamond Engine/Project Folder/Assets/Scripts/CameraController.cs
+++ Diamond Engine/Project Folder/Assets/Scripts/CameraController.cs
@@ -4,17 +4,14 @@ using DiamondEngine;
 public class CameraController : DiamondComponent
 {
 	public GameObject reference = null;
+	public GameObject target;
 
 	public float x, y, z;
-	public float rx, ry, rz;
-	public GameObject target;
 	public float smoothSpeed = 0;
-	private Vector3 offset;
 
 	public void Update()
 	{
-		offset = new Vector3(x, y, z);
-		reference.localRotation = Quaternion.ToQuaternion(rx,ry,rz);
+		Vector3 offset = new Vector3(x, y, z);
 		Vector3 desiredPosition = target.localPosition + offset;
 		Vector3 smoothPosition = Vector3.Lerp(reference.localPosition, desiredPosition, smoothSpeed);
 		reference.localPosition = smoothPosition;
