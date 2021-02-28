e8ff8f3d2ef0e02da02995bf4e21935b36dc542b
Bernat Casanas <47743689+BernatCasanas@users.noreply.github.com>
2021-02-28 18:05:15 +0100

    camera limits

--- Diamond Engine/Project Folder/Assets/Scripts/CameraController.cs
+++ Diamond Engine/Project Folder/Assets/Scripts/CameraController.cs
@@ -6,15 +6,20 @@ public class CameraController : DiamondComponent
 	public GameObject reference = null;
 	public GameObject target;
 
-	public float x, y, z;
+	public float x_offset, y_offset, z_offset;
 	public float smoothSpeed = 0;
+
+	public float x1_limit, x2_limit, y1_limit, y2_limit;
 
 	public void Update()
 	{
-		Vector3 offset = new Vector3(x, y, z);
-		Vector3 desiredPosition = target.localPosition + offset;
-		Vector3 smoothPosition = Vector3.Lerp(reference.localPosition, desiredPosition, smoothSpeed);
-		reference.localPosition = smoothPosition;
+		if (target.localPosition.x <= x1_limit && target.localPosition.x >= x2_limit && target.localPosition.y <= y1_limit && target.localPosition.y >= y2_limit)
+		{
+			Vector3 offset = new Vector3(x_offset, y_offset, z_offset);
+			Vector3 desiredPosition = target.localPosition + offset;
+			Vector3 smoothPosition = Vector3.Lerp(reference.localPosition, desiredPosition, smoothSpeed);
+			reference.localPosition = smoothPosition;
+		}
 	}
 
 }
