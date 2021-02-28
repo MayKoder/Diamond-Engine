752ef87ef42cbe44bf4a7013ab2727f634c2caa1
Bernat Casanas <47743689+BernatCasanas@users.noreply.github.com>
2021-02-28 22:52:44 +0100

    Revert "camera limits"
    This reverts commit e8ff8f3d2ef0e02da02995bf4e21935b36dc542b.

--- Diamond Engine/Project Folder/Assets/Scripts/CameraController.cs
+++ Diamond Engine/Project Folder/Assets/Scripts/CameraController.cs
@@ -6,20 +6,15 @@ public class CameraController : DiamondComponent
 	public GameObject reference = null;
 	public GameObject target;
 
-	public float x_offset, y_offset, z_offset;
+	public float x, y, z;
 	public float smoothSpeed = 0;
-
-	public float x1_limit, x2_limit, y1_limit, y2_limit;
 
 	public void Update()
 	{
-		if (target.localPosition.x <= x1_limit && target.localPosition.x >= x2_limit && target.localPosition.y <= y1_limit && target.localPosition.y >= y2_limit)
-		{
-			Vector3 offset = new Vector3(x_offset, y_offset, z_offset);
-			Vector3 desiredPosition = target.localPosition + offset;
-			Vector3 smoothPosition = Vector3.Lerp(reference.localPosition, desiredPosition, smoothSpeed);
-			reference.localPosition = smoothPosition;
-		}
+		Vector3 offset = new Vector3(x, y, z);
+		Vector3 desiredPosition = target.localPosition + offset;
+		Vector3 smoothPosition = Vector3.Lerp(reference.localPosition, desiredPosition, smoothSpeed);
+		reference.localPosition = smoothPosition;
 	}
 
 }
