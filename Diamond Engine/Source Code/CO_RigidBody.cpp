ea8201517ad81e295c3dbf6d7da3f29fdf0bcb2e
MayKoder <sg.miquel@gmail.com>
2021-03-07 12:28:57 +0100

    [CODE] PhysX fix

--- Diamond Engine/Source Code/CO_RigidBody.cpp
+++ Diamond Engine/Source Code/CO_RigidBody.cpp
@@ -110,15 +110,20 @@ void C_RigidBody::PostUpdate()
 	
 		
 		Quat rot;
-		float3 pos, scale;
-		goTransform->globalTransform.Decompose(pos, rot, scale);
-		//pos = mesh->globalOBB.pos;
-		
-
-		float4x4 pivotrans = float4x4::FromTRS(pos, rot, scale);
+		float3 pos, scale;	
+		float4x4 pivotrans = goTransform->globalTransform;
 
 		float4x4 worldtrans = pivotrans * global_to_pivot;
 		worldtrans.Decompose(pos, rot, scale);
+
+		if (DETime::state == GameState::PLAY) 
+		{
+			for (size_t i = 0; i < pos.Size; i++)
+			{
+				pos[i] = Round(pos[i] * 100) / 100;
+			}
+		}
+
 		physx::PxQuat rotation = { rot.x,  rot.y, rot.z, rot.w };
 		rigid_dynamic->setGlobalPose(physx::PxTransform({ pos.x, pos.y, pos.z }, rotation));
 
