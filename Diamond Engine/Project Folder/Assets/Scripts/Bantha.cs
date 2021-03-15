2e82a66ec6a22bfc1ae36d69efe22e1b28d3e939
Bernat Casanas <47743689+BernatCasanas@users.noreply.github.com>
2021-03-15 13:53:21 +0100

    [SCENE] Enemy Bantha ingame ready to test and polish

--- Diamond Engine/Project Folder/Assets/Scripts/Bantha.cs
+++ Diamond Engine/Project Folder/Assets/Scripts/Bantha.cs
@@ -14,18 +14,90 @@ public class Bantha : Enemy
         switch (currentState)
         {
 			case STATES.IDLE:
+				timePassed += Time.deltaTime;
+
+				if (InRange(player.transform.globalPosition, range))
+				{
+					LookAt(player.transform.globalPosition);
+
+					if (timePassed > idleTime)
+					{
+						currentState = STATES.SHOOT;
+						timePassed = timeBewteenShots;
+					}
+				}
+				else
+				{
+					if (timePassed > idleTime)
+					{
+						currentState = STATES.WANDER;
+						timePassed = 0.0f;
+						targetPosition = CalculateNewPosition(wanderRange);
+					}
+				}
 				break;
 			case STATES.RUN:
+				LookAt(targetPosition);
+				MoveToPosition(targetPosition, runningSpeed);
+
+				if (Mathf.Distance(gameObject.transform.localPosition, targetPosition) < stoppingDistance)
+				{
+					currentState = STATES.IDLE;
+					timePassed = 0.0f;
+				}
 				break;
 			case STATES.WANDER:
+				
+				// If the player is in range attack him
+				if (InRange(player.transform.globalPosition, range))
+				{
+					currentState = STATES.SHOOT;
+					timePassed = timeBewteenShots;
+				}
+				else  //if not, keep wandering
+				{
+					if (targetPosition == null)
+						targetPosition = CalculateNewPosition(wanderRange);
+
+					LookAt(targetPosition);
+					MoveToPosition(targetPosition, wanderSpeed);
+
+					if (Mathf.Distance(gameObject.transform.localPosition, targetPosition) < stoppingDistance)
+					{
+						//targetPosition = CalculateNewPosition(wanderRange);
+						currentState = STATES.IDLE;
+						timePassed = 0.0f;
+					}
+				}
 				break;
 			case STATES.SHOOT:
+				timePassed += Time.deltaTime;
+
+				LookAt(player.transform.globalPosition);
+
+				if (timePassed > timeBewteenShots)
+				{
+					Charge();
+				}
+
 				break;
 			case STATES.HIT:
 				break;
 			case STATES.DIE:
 				InternalCalls.Destroy(gameObject);
 				break;
+		}
+
+
+	}
+	public void Charge()
+    {
+		MoveToPosition(player.transform.globalPosition, bulletSpeed);
+
+		if (Mathf.Distance(gameObject.transform.localPosition, player.transform.globalPosition) < stoppingDistance)
+		{
+			currentState = STATES.IDLE;
+			timePassed = 0.0f;
 		}
 	}
 
