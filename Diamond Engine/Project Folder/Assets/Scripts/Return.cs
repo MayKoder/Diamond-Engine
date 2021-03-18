b43dc943efe871dd962d034db8304f8ecfefd8dc
MarcRosellH <36154632+MarcRosellH@users.noreply.github.com>
2021-03-18 14:22:30 +0100

    [CODE] Pause button actually pauses the game

--- Diamond Engine/Project Folder/Assets/Scripts/Return.cs
+++ Diamond Engine/Project Folder/Assets/Scripts/Return.cs
@@ -5,23 +5,16 @@ public class Return : DiamondComponent
 {
 	public GameObject controlsWindow = null;
 	public GameObject bigBrother = null;
-	private bool toDisable = false;
-	private bool firstFrame = true;
 	public void OnExecuteButton()
 	{
 		if (gameObject.Name == "Return")
-			toDisable = true;
-	}
-	public void Update()
-	{
-		if (firstFrame)
-			toDisable = firstFrame = false;
-		if (toDisable)
-		{
-			toDisable = false;
+        {
 			bigBrother.Enable(true);
 			controlsWindow.Enable(false);
 		}
+	}
+	public void Update()
+	{
 	}
 
 }
