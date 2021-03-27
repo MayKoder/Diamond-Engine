077b083561e22f125f40a37dd380e4a7d4e625e6
MayKoder <sg.miquel@gmail.com>
2021-03-27 22:16:02 +0100

    [CODE] Fix

--- Diamond Engine/Source Code/CS_Transform_Bindings.h
+++ Diamond Engine/Source Code/CS_Transform_Bindings.h
@@ -102,6 +102,9 @@ MonoObject* CS_GetComponent(MonoObject* ref, MonoString* type, int inputType)
 	Component* component = EngineExternal->moduleMono->GameObject_From_CSGO(ref)->GetComponent(sType, name);
 	mono_free(name);
 
+	if (component == nullptr)
+		return nullptr;
+
 	if (sType == Component::TYPE::SCRIPT)
 		return mono_gchandle_get_target(dynamic_cast<C_Script*>(component)->noGCobject);
 
